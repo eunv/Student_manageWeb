caf2804497bedf720181a949ef015d549b7dd4b4
eunv <dmsql1091@naver.com>
2023-01-05 19:01:22 +0900

    router-link로 변경

--- src/components/ClassA.vue
+++ src/components/ClassA.vue
@@ -3,7 +3,7 @@
     <nav class="navbar fixed-top bg-body-tertiary">
       <div class="container-fluid">
         <a class="navbar-brand" href="#">class A</a>
-        <button class="nav-link active" aria-current="page" @click = "gotoHome">Home</button>
+        <router-link to="/teacherHome">Home</router-link>
       </div>
     </nav>
 
